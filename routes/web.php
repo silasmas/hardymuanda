59b12eb8d9d9c397c46f3c721c250cf7c19670f9
silasmas <silasjmas@gmail.com>
2022-09-06 21:02:12 +0100

    ok

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.hom');
 });
+
 Route::get('/home', function () {
     return view('pages.hom');
 })->name('home');
